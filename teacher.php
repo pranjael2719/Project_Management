3801821bad3c964487e91a5063cbf2fa2b2311f2
Tichkoo27 <mail.yashrkt27@gmail.com>
2020-10-06 01:03:23 +0530

    student.php and teacher.php added

---
+++ teacher.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+    <head>
+        <title>
+            Bank Loan Management
+        </title>
+    </head>
+    <body style="background-color: orange;  text-align: center; margin-top: 150px;" >
+        <div id="bdiv" style="font-size: large; font-weight: bolder; display: inline-block; border: 1px solid #add8e6; border-radius: 4px; box-sizing: border-box; padding: 20px; background-color: #add8e6;">
+                <b><h2 style='text-align: center; color:brown;'>ABC Bank Loan Management</h2></b>
+                <form >
+                    <p>Teacher!!!</p>
+                </form>
+        </div>
+    </body>
+</html>
