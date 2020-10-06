ca0c940dc847ad2dcba589bdae2896cef4fcd434
Tichkoo27 <mail.yashrkt27@gmail.com>
2020-10-06 12:48:08 +0530

    validation is done on login page

--- login_page.php
+++ login_page.php
@@ -6,6 +6,35 @@
     </title>
     <link rel="stylesheet" href="main.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+	<script>
+	function check(){
+	var s=0;
+	var n = document.forms["login"]["username"].value;
+	if(n!='student' && n!='teacher'){
+		s=1;
+		document.getElementById('email1').style.borderColor = 'red';
+		document.getElementById('inuser').innerHTML = 'Invalid Username';
+		document.getElementById('inuser').style.color = 'red';
+	}
+	else{
+		document.getElementById('email1').style.borderColor = 'black';
+		document.getElementById('inuser').innerHTML = null;
+	}
+	var n = document.forms["login"]["password"].value;
+	if(n!='admin'){
+		s=1;
+		document.getElementById('pass').style.borderColor = 'red';
+		document.getElementById('inpass').innerHTML = 'Invalid Password';
+		document.getElementById('inpass').style.color = 'red';
+		}
+	else{
+		document.getElementById('pass').style.borderColor = 'black';
+		document.getElementById('inuser').innerHTML = null;
+	}
+	if(s==0)return true;
+	return false;
+	}
+	</script>
 </head>
 <body>
 <div class="container-fluid">
@@ -19,14 +48,16 @@
                             <div style="text-align: center;">
                                 <h3>Login to Continue</h3><br>
                             </div>
-                            <form action="" method="POST" onsubmit="return check()">
+                            <form action="" method="POST" onsubmit="return check()" name='login'>
                                 <div class="form-group mb-3">
                                     <label for="email1">User ID </label>
                                     <input type="text" class="form-control" id="email1" aria-describedby="emailHelp" style="border-radius: 40px" placeholder="User ID" required name="username">
+                                    <p id='inuser'></p>
                                 </div>
                                 <div class="form-group mb-3">
                                     <label for="pass">Password</label>
                                     <input type="password" class="form-control" id="pass" style="border-radius: 40px" placeholder="Password" required name="password">
+									<p id='inpass'></p>
                                 </div>
                                 <div class="custom-control custom-checkbox mb-3">
                                     <input id="customCheck1" type="checkbox" class="custom-control-input">
