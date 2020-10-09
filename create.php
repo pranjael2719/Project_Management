b2231eece8be5e59288f1f811976084d747c7845
InsideU <umang20anku@gmail.com>
2020-10-09 20:02:06 +0530

    added formteam

--- create.php
+++ create.php
@@ -15,9 +15,7 @@ if ($count != 0) {
         echo $row["Name"] . "<br>";
     }
 } else {
-    $rand_number = rand(100000, 10000000);
-    $secret_key = base64_encode($rand_number);
-    #echo $secret_key;
+   
 ?>
     <!DOCTYPE html>
     <html lang="en">
