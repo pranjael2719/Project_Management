08da9ef6b47a9f45610f97d90ca31a829ac7d132
InsideU <umang20anku@gmail.com>
2020-10-09 12:26:27 +0530

    last change

--- student.php
+++ student.php
@@ -11,10 +11,11 @@
             $myusername=$_SESSION['login_user'];
             $sql = "SELECT teach_sub.Subject FROM teach_sub INNER JOIN auto_role ON teach_sub.Auto=auto_role.Auto WHERE roll_no='$myusername'";
             $result = mysqli_query($db,$sql);
-            if (!$result) {
-                printf("Error: %s\n", mysqli_error($db));
-                exit();
-            }
+            //Error handling for the database 
+            // if (!$result) {
+            //     printf("Error: %s\n", mysqli_error($db));
+            //     exit();
+            // }
             while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){
               
                 echo $row["Subject"];
