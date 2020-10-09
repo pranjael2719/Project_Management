136e50b716e43c80016bf34530ad5f0a17c5557d
InsideU <umang20anku@gmail.com>
2020-10-09 18:11:27 +0530

    join team using secret key

--- jointeam.php
+++ jointeam.php
@@ -1,4 +1,30 @@
 <?php
-    echo $_POST['secret'];
+    session_start();
+    include ("config.php");
+
+    echo $_SESSION['login_user'];
+    // getting the secret key for the user
+    
+    $secret = $_POST['secret'];
+
+    // Now insert the record into the database 
+
+    $sql = "SELECT Team_Name from project_code where Group_Code='$secret'";
+    $result = mysqli_query($db, $sql);
+    $row = mysqli_fetch_array($result);
+
+    $team_name = $row['Team_Name'];
+
+    $sql1 = "INSERT INTO project_code(Group_Code,Team_Name) values('$secret','$team_name')";
+    
+    if($db->query($sql1) ===TRUE){
+        echo "New Record Inserted";
+    }
+    else{
+        echo "Error: " . $sql . "<br>" . $db->error;
+    }
+
+
+
     
 ?>
