eaf9204d8ece138a9a68b740f019d60d526d9508
Pranjael Bagaria <pranjael2719@gmail.com>
2020-10-09 12:25:31 +0530

    Added cards to student.php

--- student.php
+++ student.php
@@ -3,6 +3,7 @@
         <title>
             Student
         </title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     </head>
     <body>
         <?php 
@@ -15,9 +16,14 @@
                 printf("Error: %s\n", mysqli_error($db));
                 exit();
             }
-            while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){
-              
-                echo $row["Subject"];
+            while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)){ ?>
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $row["Subject"]; ?></h5>
+                        <a href="create.php" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <?php
             }
         ?>
     </body>
