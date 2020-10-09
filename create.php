d219807e55a74e87a3db961e74644833b1327a9a
Pranjael Bagaria <pranjael2719@gmail.com>
2020-10-09 13:22:37 +0530

    if-else added in create.php

--- create.php
+++ create.php
@@ -1,8 +1,18 @@
 <?php
- echo $_GET['Auto'];
- $rand_number = rand(100000,10000000);
- $secret_key = base64_encode($rand_number);
- echo $secret_key;
-
- 
+    session_start();
+    include ("config.php");
+    $Auto=$_GET['Auto'];
+    $myusername=$_SESSION["login_user"];
+    $sql = "SELECT secret_key FROM auto_role WHERE roll_no = '$myusername' and secret_key!='NULL' AND auto_role.Auto='$Auto' ";
+    $result = mysqli_query($db,$sql);
+    $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+    $count = mysqli_num_rows($result);
+    if ($count==0){
+        //displaying the team members
+    }
+    else{
+        $rand_number = rand(100000,10000000);
+        $secret_key = base64_encode($rand_number);
+        echo $secret_key; 
+    }
 ?>
