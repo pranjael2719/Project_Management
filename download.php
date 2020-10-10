da57d8d065297bad3894bcab8da16776ad760b85
Pranjael Bagaria <pranjael2719@gmail.com>
2020-10-10 10:02:05 +0530

    File Donwload

--- download.php
+++ download.php
@@ -2,7 +2,10 @@
     session_start();
     include("config.php");
     $Auto = $_SESSION['sno'];
-    
+    header('Content-Type: text/csv; charset=utf-8');
+    header('Content-Disposition: attachment; filename=data.csv');
+    $output = fopen('php://output', 'w');
+    fputcsv($output, array("Group_No","Project Title",'Name', 'Registration No'));
     $myusername = $_SESSION["login_user"];
     $sql="SELECT DISTINCT Group_Code from project_code WHERE Sr_No='$Auto'";
     $result = mysqli_query($db,$sql);
@@ -14,29 +17,11 @@
         }
         $sql1="SELECT project_code.Team_Name,student.Name,student.Roll_No FROM student INNER JOIN project_code ON student.Roll_no=project_code.Roll_no WHERE project_code.Group_Code='$group' AND project_code.Sr_No='$Auto' ";
         $result1 = mysqli_query($db, $sql1);
-        echo "Group ".$a."<br>";
-        $flag=0;
-        while ($row1 = mysqli_fetch_array($result1)) {
-            if ($flag==0){
-                echo "Project Title: ".$row1["Team_Name"]."<br>";
-                $flag=1;
-            }
-            echo $row1["Name"] ."  ".$row1["Roll_No"]."<br>";
+        while ($row1 = mysqli_fetch_array($result1,MYSQLI_ASSOC)) {
+            array_unshift($row1,$a);
+            fputcsv($output, $row1);
         }
         $a=$a+1;
     }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <br>
-    <form action="download.php">
-        <input type="submit" value="Download" >
-    </form>
-</body>
-</html>
+
